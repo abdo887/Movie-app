2e487c588a3cc4d4772838ac8fee9937c51f7cb0
abdo887 <abdoraga52@gmail.com>
2022-10-19 16:46:30 +0200

    Adding Final things

--- src/compoents/input/Input.jsx
+++ src/compoents/input/Input.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const Input = () => {
-  return (
-    <div>Input</div>
-  )
+import './input.scss';
+
+const Input = props => {
+    return (
+        <input
+            type={props.type}
+            placeholder={props.placeholder}
+            value={props.value}
+            onChange={props.onChange ? (e) => props.onChange(e) : null}
+        />
+    );
 }
 
-export default Input
+export default Input;
